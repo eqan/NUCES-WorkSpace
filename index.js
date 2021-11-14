f304d87d15128f34b26f0da9b4e7fb9875920825
eqan <eqan.ahmad123@gmail.com>
2021-11-14 21:00:07 +0500

    Initial CRUD Operations Commit + Cleaning

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const mysql = require('mysql')
+
+// Create Connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'WorkSpaceDB'
+
+})
+
+// Connect to MySQL
+db.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log("SQL Connected!");
+})
+
+const app = express();
+
+// Create Database
+app.get('/createdb', (req, res) => {
+  let cmd = 'CREATE DATABASE WorkSpaceDB';
+  db.query(cmd, err => {
+    if (err)
+      throw err;
+    res.send("Database Created!");
+  })
+})
+
+
+// Create Table
+app.get('/createusertable', (req, res) => {
+  let cmd = 'CREATE TABLE Users(id int AUTO_INCREMENT, username VARCHAR(255), name VARCHAR(255),'
+    + 'password VARCHAR(255), authoritylevel int, PRIMARY KEY(id))';
+  db.query(cmd, err => {
+    if (err)
+      throw err;
+    res.send("Employee Table Created!");
+  })
+})
+
+
+// Insert User
+app.get('/insertuser', (req, res) => {
+  let post = { username: 'eqan', name: 'Eqan Ahmad', password: 'eqan', authoritylevel: '0' }
+  let cmd = 'INSERT INTO Users SET ?';
+  let query = db.query(cmd, post, err => {
+    if (err)
+      throw err;
+    res.send("User Inserted!");
+  })
+})
+
+// Select Users
+app.get('/getusers', (req, res) => {
+  let cmd = 'SELECT * FROM Users';
+  let query = db.query(cmd, (err, results) => {
+    if (err)
+      throw err;
+    console.log(results);
+    res.send("Users Displayed!");
+  })
+})
+
+// Update User
+app.get('/updateuser/:id', (req, res) => {
+  let newName = "Ali Ahmad";
+  let cmd = `UPDATE Users SET name = '${newName}' WHERE id = ${req.params.id}`;
+  let query = db.query(cmd, err => {
+    if (err)
+      throw err;
+    res.send("User Updated!");
+  })
+})
+
+// Delete User
+app.get('/deleteuser/:id', (req, res) => {
+  let cmd = `DELETE FROM Users WHERE id = ${req.params.id}`;
+  let query = db.query(cmd, err => {
+    if (err)
+      throw err;
+    res.send("User Deleted!");
+  })
+})
+
+app.listen('3000', () => {
+  console.log('Server Started on port 3000');
+})

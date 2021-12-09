89a2f8862e09ebe8cc1f3b5302a4849a9818b118
M Shahid Imran <mshahidimran@gmail.com>
2021-12-09 22:17:22 +0500

    React Installed!

---
+++ forntend/src/Components/forms/registration.js
@@ -0,0 +1,12 @@
+import './registration.css';
+import React from 'react';
+
+const registration=()=>{
+    
+    return (
+        <div>
+            <h1>Hello I'm making registration</h1>
+        </div>
+    )
+};
+export default registration;

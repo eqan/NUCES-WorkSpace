2022031786050eedf787ce166769801b639c562f
eqan <eqan.ahmad123@gmail.com>
2022-01-19 20:42:17 +0500

    Refactoring some code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component, useState } from 'react';
+import Register from './views/register'
+import Login from './views/login'
+import Researchport from './views/faculty/regprot'
+import Academicport from './views/faculty/acdport'
+import Course from './views/faculty/coursefold'
+import {Route, Switch} from 'react-router-dom'
+
 function App() {
     return (
+        <Switch>
+            <Route path="/fac_research" component={Researchport}/>
+            <Route path="/fac_acad" component={Academicport}/>
+            <Route path="/fac_course" component={Course}/>
+            <Route path="/register" component={Register}/>
+            <Route path="/" component={Login}/>
+        </Switch>
     )
 }
 

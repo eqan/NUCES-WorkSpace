a0db38a661d7dc01e975a2438d84e290b4bf675c
eqan <eqan.ahmad123@gmail.com>
2022-01-25 00:20:21 +0500

    More configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Register from './views/register'
-import Login from './views/login'
-import Researchport from './views/faculty/regprot'
-import Academicport from './views/faculty/acdport'
-import Course from './views/faculty/coursefold'
+import Register from './views/register';
+import Login from './views/login';
+import NotFound from './views/notfound';
+import Researchport from './views/faculty/regprot';
+import Academicport from './views/faculty/acdport';
+import Course from './views/faculty/coursefold';
 import { Routes, Route} from "react-router-dom";
+import { NotImplemented } from 'http-errors';
 
 function App() {
     return (
@@ -12,8 +14,10 @@ function App() {
             <Route exact path="/faculty/research" element={<Researchport/>}/>
             <Route exact path="/faculty/academic" element={<Academicport/>}/>
             <Route exact path="/faculty/course" element={<Course/>}/>
+            <Route path="/" element={<Login/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
+            <Route path='*' exact={true} element={<NotFound/>} />
         </Routes>
     )
 }
